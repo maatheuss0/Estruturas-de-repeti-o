bd13d2591d21565bc62eea43b46181f2c3781249
DEV <dev@info.local>
2021-05-05 10:24:58 -0300

    C#

---
+++ informações_C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace informações_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool resposta = false;
+            do
+            {
+                Console.WriteLine("Escreva seu nome: ");
+                string respostaNome = Console.ReadLine();
+                
+                Console.WriteLine("Escreva sua idade: ");
+                int respostaIdade = int.Parse(Console.ReadLine());
+                
+                Console.WriteLine("Escreva seu salário: ");
+                int respostaSalario = int.Parse(Console.ReadLine());
+                
+                Console.WriteLine("Escreva seu estado civil: ");
+                string respostaCivil = Console.ReadLine();
+
+                
+                
+            } while (true);
+        }
+    }
+}
---
+++ nota_C#/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace nota_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool resposta = false;
+            do
+            {
+                Console.WriteLine("Escreva um número de 0 a 10");
+                int numero = int.Parse(Console.ReadLine());
+
+                if ( numero > 10)
+                {
+                    Console.WriteLine("Número inválido!!!");
+                }
+
+                else
+                {
+                    Console.WriteLine("Número válido");
+                    Console.WriteLine("Execução Finalizada!!!");
+                    resposta = true;
+                }
+                
+
+            } while (resposta == false);
+        }
+    }
+}
