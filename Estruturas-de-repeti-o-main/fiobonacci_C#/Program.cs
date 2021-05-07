b0a3fbcc13a78f6b7f999fc9f403f5b96f91fb7b
maatheuss0 <82424913+maatheuss0@users.noreply.github.com>
2021-05-07 08:47:35 -0300

    Add files via upload

---
+++ Estruturas-de-repeti-o-main/fiobonacci_C#/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fiobonacci_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroAnterior = -1;
+            int proximoNumero = 1;
+            int resultado;
+
+            for (var i = 0; i < 15; i++)
+            {
+                resultado = numeroAnterior + proximoNumero;
+                numeroAnterior = proximoNumero;
+                proximoNumero = resultado;
+
+                Console.WriteLine( ",  ");
+            }
+        }
+    }
+}
---
+++ Estruturas-de-repeti-o-main/informações_C#/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace informações_C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool resposta = false;
+            do
+            {
+                Console.WriteLine("Escreva seu nome: ");
+                string respostaNome = Console.ReadLine();
+
+                if (respostaNome == "")
+                {
+                    Console.WriteLine("Por favor, insira um nome existente");
+                    respostaNome = Console.ReadLine();
+                }
+                else
+                {
+                    resposta = true;
+                }
+
+            } while (resposta == false);
+
+            bool idadee = false;
+            do
+            {
+                Console.WriteLine("Insira sua Idade: ");
+                int idade = int.Parse(Console.ReadLine());
+
+                if (idade >= 150)
+                {
+                    Console.WriteLine("Idade incorreta!!!");
+                }
+                else
+                {
+                    idadee = true;
+                }
+
+            } while (idadee == false);
+
+            bool salario = false;
+            do
+            {
+                Console.WriteLine("Insira seu salário: ");
+                int dinheiro = int.Parse(Console.ReadLine());
+
+                if (dinheiro <= 0)
+                {
+                    Console.WriteLine("Salário incorreto");
+                    Console.WriteLine("Insira um salário maior que R$0: ");
+                }
+                else
+                {
+                    salario = true;
+                }
+
+            } while (salario == false);
+
+            Console.WriteLine("Digite seu estado civil s(solteiro(a)), c(casado(a)), v(viuvo(a)), d(divorciado(a)): ");
+            estadoCivil = Console.ReadLine();
+            estadoCivilV = false;
+
+            while (estadoCivil == "s" || estadoCivil == "S")
+            {
+                estadoCivilV = true;
+            }
+        }
+    }
+}
